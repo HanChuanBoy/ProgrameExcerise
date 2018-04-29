c861af5741a1cbfa5613bec781aeb571852f7594
hang.xu <zhiyanzhai563@163.com>
2018-04-29 16:00:30 +0800

    add running error and array init is not const but copy string

--- 30-question-writing-test-xianfengshangtai/jichu2.cpp
+++ 30-question-writing-test-xianfengshangtai/jichu2.cpp
@@ -7,12 +7,13 @@ int main()
 char* s1 = "string";
 char s2[] = "string";
 	s1 = "w";
-	*s1 = 'w';
-	s2 = "wlllll";
+	//*s1 = 'w';
+	//s2 = "wlllll";
 	*s2 = 'w';
 }
 /*c99
 11:cannot convert from 'const char [7]' to 'char [7]'
         There is no context in which this conversion is possible
+jichu2:10 running error  
 
 */
