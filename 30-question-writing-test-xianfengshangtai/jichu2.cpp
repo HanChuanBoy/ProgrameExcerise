fc792f453016e4ae1c93e6741122d761f06ef6e7
hang.xu <zhiyanzhai563@163.com>
2018-04-29 15:35:16 +0800

    char s[]= type s is const char [7]

---
+++ 30-question-writing-test-xianfengshangtai/jichu2.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+
+
+int main()
+{
+char* s1 = "string";
+char s2[] = "string";
+	s1 = "w";
+	*s1 = 'w';
+	s2 = "wlllll";
+	*s2 = 'w';
+}
+/*c99
+11:cannot convert from 'const char [7]' to 'char [7]'
+        There is no context in which this conversion is possible
+
+*/
